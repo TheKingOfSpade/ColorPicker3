7c3ef54619094489b2351d889dfea9b648cb4f8b
aw9p <aw9p@fresnou.org>
2023-09-01 09:10:46 -0700

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,31 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-    color: "#FFFFFF"
+    color: "#FFFFFF",
+    textColor: "#000000"
   });
 });
 
-
 router.post('/', function (req, res){
 res.render('index', {
-  color: req.body.colorPicker
-})
-} )
+  color: req.body.colorPicker,
+  textColor:req.body.textColorPicker
+  })
+});
+
+function generateRandomhexCode() {
+  let hexCode = "#"
+  while (hexCode.length < 7 ) {
+    hexCode += (Math.round( Math.random() * 15)).toString(16)
+  }
+  return hexCode
+}
+
+router.post('/random', function (req, res){
+  res.render('index', {
+    color: generateRandomhexCode(),
+    textColor: generateRandomhexCode()
+  })
+});
+
 module.exports = router;
